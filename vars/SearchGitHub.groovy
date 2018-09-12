bf1d766cd182882507d1fece448c31f4d09485e4
Tito Mathews <tito.mathews@gmail.com>
2018-09-11 20:07:18 -0600

    updated

--- vars/SearchGitHub.groovy
+++ vars/SearchGitHub.groovy
@@ -2,7 +2,7 @@
 //vars/SearchGitHub.groovy
 
 def call(String s) {
-  String url = "https://api.github.com/search/code?q=${s} in:file&access_token=b44984164255e21780ababda4888acad0f97eb4d";
+  String url = "https://api.github.com/search/code?q=${s}%20in:file&access_token=b44984164255e21780ababda4888acad0f97eb4d";
   def connection = new URL(url).openConnection() as HttpURLConnection;
   // set some headers
   connection.setRequestProperty( 'User-Agent', 'groovy-2.4.4' )
